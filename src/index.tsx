1f74fb9266c1db48d51fc717615b89775271fa44
Tuan Pham <tuanpham.developer@gmail.com>
2020-04-10 09:14:28 -0400

    Add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import App from './components/App/App'
+import './sass/global.sass'
+
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
